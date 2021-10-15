aa681e68c7d1fed9c934264419398cb3f98248ce
root <root@gaokakao.com>
2021-10-15 17:47:38 +0300

    Counter Increment working

--- gaokakao/src/components/Counter.jsx
+++ gaokakao/src/components/Counter.jsx
@@ -3,32 +3,53 @@ import 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
 class Counter extends React.Component
 {
-    state=
+    state =
         {
             count: 0,
+            tags: [ 4, 8, 15, 16, 23, 42 ]
         };
-    render()
+
+    renderTags ()
+    {
+        if ( this.state.tags.length === 0 ) return <p> Tehere are no tags</p>;
+        return <ul> { this.state.tags.map( tag => <li key={ tag }>{ tag }</li> ) } </ul>;
+    }
+
+    handleIncrement = () =>
+    {
+        this.setState( { count: this.state.count + 1 } );
+    }
+    render ()
     {
         return (
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn-primary btn-sm m-4">Increment </button>
+                <img src="" />
+                <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+                <button onClick={ () => this.handleIncrement( 1 ) } className="btn-primary btn-sm m-5">Increment </button>
 
-
+                { this.state.tags.length === 0 && 'Please Create a new tag' }
+                { this.renderTags() }
             </div>
 
         );
     }
-    getBadgeClasses()
+    getbadgeClasses ()
     {
-        let classes="badge m2 bg-";
-        classes+=this.state.count===0? "warning":"primary";
+        let classes = "badge m-2  bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
     }
 
-    formatCount()
+    getBadgeClasses ()
     {
-        return this.state.count===0? <h3> Zero</h3>:this.state.count;
+        let classes = "badge m2 bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount ()
+    {
+        return this.state.count === 0 ? <h3> Zero</h3> : this.state.count;
     }
 
 }
